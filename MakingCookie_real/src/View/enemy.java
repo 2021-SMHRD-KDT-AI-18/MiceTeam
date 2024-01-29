4049d270512082333c21640598657e2a85450453
smhrd <smhrd@59.0.129.84>
2024-01-29 09:05:42 +0900

    enemy 추상 메소드

---
+++ MakingCookie_real/src/View/enemy.java
@@ -0,0 +1,23 @@
+package View;
+
+public abstract class enemy {
+
+	int enemyAtk;
+	int enemyHp;
+	String name;
+	
+	public abstract void fight();
+		    
+		
+		
+				
+		
+	
+	
+	
+	
+	
+	
+	
+	
+}
